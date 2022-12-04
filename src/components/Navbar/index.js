e24b20a1951613dd0f856c96d15409de29ad07d5
avinash gupta <gavinash6290@gmail.com>
2022-12-04 10:08:10 +0530

    navbar-responsiveness

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,14 +6,26 @@ const Navbar = () => {
             <div className='main-wrapper nav-container bg-blur'>
                 <nav className='w-75 m-auto navbar'>
                     <h1 className='logo'>devlopify</h1>
-                    <ul>
+                    <ul className='p-0'>
                         <a className='nav-items'>Home</a>
                         <a className='nav-items c-grey'>Features</a>
                         <a className='nav-items c-grey'>Testimonials</a>
                         <a className='nav-items c-grey'>Pricing</a>
                     </ul>
                     <button className='btn-primary'>Contact</button>
+                    <button className='bg-none' type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample"><i className="fa-solid fa-bars"></i></button>
                 </nav>
+            </div>
+            <div className="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                <div className='nav-responsive'>
+                    <ul className='p-0'>
+                        <a className='nav-items'>Home</a>
+                        <a className='nav-items c-grey'>Features</a>
+                        <a className='nav-items c-grey'>Testimonials</a>
+                        <a className='nav-items c-grey'>Pricing</a>
+                    </ul>
+                    <button className='btn-primary'>Contact</button>
+                </div>
             </div>
         </>
     );
