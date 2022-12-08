87133b0cc063d8df66f9529e22129f84ea57b78f
avinash gupta <gavinash6290@gmail.com>
2022-12-08 14:40:56 +0530

    Form link

--- src/components/Careers/index.js
+++ src/components/Careers/index.js
@@ -9,7 +9,7 @@ const Careers = () => {
                 <h4>Then for what you are waiting. <i>Come join our team today</i>.</h4>
                 <p>All you need to do is just to fill this form powered by <i>Google</i> and just sit. We will reach you once your resume got shortlisted.</p>
 
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLScrnvmqJd9-83LTquRt_XslT5Y7fVcDZu3KG0HsCX8cZy6ziQ/viewform"><i>Hiring Form</i></a>
+                <a href="https://forms.gle/iKsFFJBcKQL8xsmf6"><i>Hiring Form</i></a>
 
                 <p className="note">Note, the hiring process may take some time in order to reach you. In that case we expect a quite patience from your side.</p>
             </div>
